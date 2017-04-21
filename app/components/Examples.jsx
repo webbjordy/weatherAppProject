2e7a6d8224643b58f4889f3be4e10bca7befdc28
Jordy Webb <webb.jordy@gmail.com>
2017-04-21 06:24:42 -0700

    add grid system and style examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,19 @@
 var React = require('react');
+var { Link } = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h2>Examples</h2>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few examples to try:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Portland'>Portland, OR</Link>
+        </li>
+        <li>
+          <Link to='/?location=Seattle'>Seattle, WA</Link>
+        </li>
+      </ol>
     </div>
   )
 }
--- public/bundle.js
+++ public/bundle.js
@@ -24921,11 +24921,14 @@
 	    null,
 	    React.createElement(Nav, null),
 	    React.createElement(
-	      'h2',
-	      null,
-	      'Main component'
-	    ),
-	    props.children
+	      'div',
+	      { className: 'row' },
+	      React.createElement(
+	        'div',
+	        { className: 'columns medium-6 large-4 small-centered' },
+	        props.children
+	      )
+	    )
 	  );
 	};
 
@@ -28769,14 +28772,44 @@
 
 	var React = __webpack_require__(8);
 
+	var _require = __webpack_require__(166),
+	    Link = _require.Link;
+
 	var Examples = function Examples(props) {
 	  return React.createElement(
 	    'div',
 	    null,
 	    React.createElement(
-	      'h2',
-	      null,
+	      'h1',
+	      { className: 'text-center' },
 	      'Examples'
+	    ),
+	    React.createElement(
+	      'p',
+	      null,
+	      'Here are a few examples to try'
+	    ),
+	    React.createElement(
+	      'ol',
+	      null,
+	      React.createElement(
+	        'li',
+	        null,
+	        React.createElement(
+	          Link,
+	          { to: '/?location=Portland' },
+	          'Portland, OR'
+	        )
+	      ),
+	      React.createElement(
+	        'li',
+	        null,
+	        React.createElement(
+	          Link,
+	          { to: '/?location=Seattle' },
+	          'Seattle, WA'
+	        )
+	      )
 	    )
 	  );
 	};
